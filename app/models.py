6ee0c0cfbcef9d5433f94f89ce25a962114adfdc
ashuza <ashuzamh@gmail.com>
2025-09-06 00:52:11 +0100

    end six and seven

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from hashlib import md5
 from typing import Optional
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,11 +16,20 @@ class User(UserMixin, db.Model):
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)  # type: ignore
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
     def __repr__(self):
         return "<User {}>".format(self.username)
